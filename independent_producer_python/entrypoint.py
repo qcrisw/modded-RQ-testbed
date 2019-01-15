a5456f9535dfd6fd6d7dac8fafe6fdf4d450a7bb
ahmad-musleh <ahmad.s.musleh@gmail.com>
2019-01-15 16:18:55 +0300

    Change payload structure and ensure arg type as bytes

--- independent_producer_python/entrypoint.py
+++ independent_producer_python/entrypoint.py
@@ -1,22 +1,24 @@
 from uuid import uuid4
 from redis import StrictRedis
 from datetime import datetime
+from json import dumps
 import logging
 
 
 def create_mapping(func_name, arg):
     mapping = {}
-    # add job status
     mapping['created_at'] = datetime.strftime(datetime.now(), '%Y-%m-%dT%H:%M:%S.%fZ')
-    mapping['data'] = func_name + b' ' + arg
+    mapping['func_name'] = func_name
+    mapping['arg'] = arg
     mapping['origin'] = 'default'
-    mapping['description'] = func_name+b'('+arg+b')'
+    mapping['description'] = f"{func_name.encode('utf-8')}({arg})"
     mapping['timeout'] = 180
     mapping['raw'] = "True"
     return mapping
 
 # create RQ job to push to redis
 def create_job(func_name, arg):
+    assert type(arg) == bytes
     job_data = create_mapping(func_name, arg)
     job_uuid = uuid4()
     job_name = str(job_uuid)
@@ -38,10 +40,8 @@ def enqueue_job(redis_client, job_id, mapping):
 
 
 if __name__=="__main__":
-    #create task
-    #enqueue to redis
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
     client = connect_to_redis()
-    job_id, mapping = create_job(b'consumer1.consume.consume_func', b"4")
+    job_id, mapping = create_job('consumer1.consume.consume_func',  b'{"a": "b"}')
     enqueue_job(client, job_id, mapping)
