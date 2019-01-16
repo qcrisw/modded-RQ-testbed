3286580d56477defeee0e9a82356ce3f9a566054
ahmad-musleh <ahmad.s.musleh@gmail.com>
2019-01-16 14:14:48 +0300

    Create Independent Queue class
    Mimic enqueue method from RQ library.

--- independent_producer_python/entrypoint.py
+++ independent_producer_python/entrypoint.py
@@ -5,43 +5,60 @@ from json import dumps
 import logging
 
 
-def create_mapping(func_name, arg):
-    mapping = {}
-    mapping['created_at'] = datetime.strftime(datetime.now(), '%Y-%m-%dT%H:%M:%S.%fZ')
-    mapping['func_name'] = func_name
-    mapping['arg'] = arg
-    mapping['origin'] = 'default'
-    mapping['description'] = f"{func_name.encode('utf-8')}({arg})"
-    mapping['timeout'] = 180
-    mapping['raw'] = "True"
-    return mapping
+class IndependentQueue():
+    redisClient = None
+    queueName = None
 
-# create RQ job to push to redis
-def create_job(func_name, arg):
-    assert type(arg) == bytes
-    job_data = create_mapping(func_name, arg)
-    job_uuid = uuid4()
-    job_name = str(job_uuid)
-    return (job_name, job_data)
+    RQ_QUEUE_PREFIX = "rq:queue:"
+    RQ_JOB_PREFIX = "rq:job:"
 
-def connect_to_redis():
-    redis_client = StrictRedis('redis')
-    return redis_client
-
+    def __init__(self, queueName='default', redisClient=None):
+        # initialize queue
+        self.queueName = queueName
+        if redisClient is None:
+            raise ValueError("No redis client provided")
+        self.redisClient = redisClient
+        self.redisClient.sadd("rq:queues", self.RQ_QUEUE_PREFIX+str(queueName))
     
-def enqueue_job(redis_client, job_id, mapping):
-    mapping['status'] = 'queued'
-    mapping['enqueued_at'] = datetime.strftime(datetime.now(), '%Y-%m-%dT%H:%M:%S.%fZ')
-    # mapping['enqueued_at'] = datetime.now().isoformat()
-    redis_client.hmset("rq:job:"+job_id, mapping)
-    redis_client.rpush('rq:queue:default', job_id)
-    redis_client.sadd('rq:queues', 'rq:queue:default')
+    # create RQ job to push to redis
+    def create_job(self, func_name, arg, timeout=180, result_ttl=500, 
+                    ttl=420, description=None, depends_on=None,
+                    job_id=None, at_front=False, meta=None, raw=False):
+        assert type(arg) == bytes
+        mapping = {}
+        mapping['created_at'] = datetime.strftime(datetime.now(), '%Y-%m-%dT%H:%M:%S.%fZ')
+        mapping['func_name'] = func_name
+        mapping['arg'] = arg
+        mapping['origin'] = self.queueName
+        mapping['description'] = description if description is not None else func_name
+        mapping['timeout'] = timeout
+        mapping['raw'] = str(raw)
 
+        job_name = str(uuid4())
+        return (job_name, mapping)
+
+    def enqueue(self, func_name, arg, timeout=180, 
+                    result_ttl=500, ttl=420, description=None, depends_on=None,
+                    job_id=None, at_front=False, meta=None, raw=False):
+        # create job from data provided
+        job_name, job = self.create_job(func_name, arg, timeout, result_ttl, ttl, 
+                        description, depends_on, job_id, at_front, meta, raw)
+        # enqueue job to queue
+        self.redisClient.hmset(self.RQ_JOB_PREFIX+str(job_name), job)
+        if at_front:
+            self.redisClient.lpush(self.RQ_QUEUE_PREFIX+str(self.queueName), job_name)
+        self.redisClient.lpush(self.RQ_QUEUE_PREFIX+str(self.queueName), job_name)
 
 
 if __name__=="__main__":
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
-    client = connect_to_redis()
-    job_id, mapping = create_job('consumer1.consume.consume_func',  b'{"a": "b"}')
-    enqueue_job(client, job_id, mapping)
+
+    host = 'redis'
+    port = 6379
+    db = 0
+
+    redisClient = StrictRedis(host=host, port=port, db=db)
+
+    queue = IndependentQueue(redisClient=redisClient)
+    queue.enqueue('consumer1.consume.consume_func',  b'{"a": "b"}')
