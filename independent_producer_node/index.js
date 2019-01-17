7d3b371553b0ddea215ccf39ae81c3e3037ce725
Hossam Hammady <github@hammady.net>
2019-01-17 13:25:54 +0300

    Implement NodeJS createQueue

--- independent_producer_node/index.js
+++ independent_producer_node/index.js
@@ -1,12 +1,18 @@
+const redis = require("redis")
 const rqJobGenerator = require('./rq-job-generator')
+const redisClient = redis.createClient(process.env.REDIS_URL)
 
-console.log("I am the NodeJS producer!")
+redisClient.on("error", function (err) {
+  console.log("Error " + err)
+  process.exit(1)
+});
 
-redisClient = 0
-const queue = rqJobGenerator(redisClient, 'default')
+const queue = rqJobGenerator(redisClient, process.env.IN_QUEUE)
 queue.enqueue('consumer1.consume.consume_func',  '{"a": "a.js"}', {description: 'No description!'})
 queue.enqueue('consumer1.consume.consume_func',  '{"a": "b.js"}', {timeout: 30})
 queue.enqueue('consumer1.consume.consume_func',  '{"a": "c.js"}', {result_ttl: 100})
 queue.enqueue('consumer1.consume.consume_func',  '{"a": "d.js"}', {ttl: 200, at_front: true})
 queue.enqueue('consumer1.consume.consume_func',  '{"a": "e.js"}', {})
 queue.enqueue('consumer1.consume.consume_func',  '{"a": "f.js"}')
+
+redisClient.quit(function(){ process.exit(0) })
--- independent_producer_node/rq-job-generator.js
+++ independent_producer_node/rq-job-generator.js
@@ -2,8 +2,20 @@ module.exports = function _rqJobGenerator(redisClient, queueName) {
   return new function () {
     const me = this
 
+    if (!queueName) {
+      throw("queueName cannot be empty")
+    }
+
+    const RQ_QUEUE_NAME = `rq:queue:${queueName}`
+    const RQ_JOB_PREFIX = "rq:job:"
+
     const createQueue = function _createQueue(queueName) {
       console.log("Should create queue here with name: %s", queueName)
+      // initialize queue
+      if (!redisClient) {
+        throw("No redis client provided")
+      }
+      redisClient.sadd("rq:queues", RQ_QUEUE_NAME)
     }
 
     createQueue(queueName)
