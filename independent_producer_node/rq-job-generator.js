b4fc672f02c62a74a32646d552250d2b554a1fdd
Hossam Hammady <github@hammady.net>
2019-01-17 15:10:32 +0300

    Implement NodeJS enqueue

--- independent_producer_node/rq-job-generator.js
+++ independent_producer_node/rq-job-generator.js
@@ -1,3 +1,5 @@
+const uuidv4 = require('uuid/v4')
+
 module.exports = function _rqJobGenerator(redisClient, queueName) {
   return new function () {
     const me = this
@@ -10,15 +12,31 @@ module.exports = function _rqJobGenerator(redisClient, queueName) {
     const RQ_JOB_PREFIX = "rq:job:"
 
     const createQueue = function _createQueue(queueName) {
-      console.log("Should create queue here with name: %s", queueName)
       // initialize queue
       if (!redisClient) {
         throw("No redis client provided")
       }
       redisClient.sadd("rq:queues", RQ_QUEUE_NAME)
+      console.log("Created queue with name: %s", queueName)
+      // TODO callback
     }
 
     createQueue(queueName)
+
+    // create RQ job to push to redis
+    const createJobMapping = function _createJobMapping(funcName, arg, options) {
+      // TODO assert type(arg) == bytes
+      return {
+        created_at: new Date().toISOString(),
+        func_name: funcName,
+        arg: arg,
+        origin: queueName,
+        description: options.description,
+        timeout: options.timeout,
+        raw: 'True' // TODO is this a string?
+      }
+      // TODO where are the rest of options consumed?
+    }
 
     this.enqueue = function _enqueue(funcName, arg, options) {
       const _options = Object.assign({
@@ -29,7 +47,19 @@ module.exports = function _rqJobGenerator(redisClient, queueName) {
         at_front: false
       }, options || {})
 
-      console.log("Should enqueue job here: %s(%s) with options: %o", funcName, arg, _options)
+      const job_name = uuidv4(),
+            mapping = createJobMapping(funcName, arg, _options)
+
+      // enqueue job to queue
+      redisClient.hmset(`${RQ_JOB_PREFIX}${job_name}`, mapping)
+      if (_options.at_front)
+          redisClient.lpush(RQ_QUEUE_NAME, job_name)
+      else
+          redisClient.rpush(RQ_QUEUE_NAME, job_name)
+
+      // TODO callbacks
+
+      console.log("Enqueued job: %s(%s) with options: %o", funcName, arg, _options)
     }
   }
 }
